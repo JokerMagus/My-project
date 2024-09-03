6353ae282021c6a045a3a796bb77bb8ea8aae4de
JokerMagus <140458247+JokerMagus@users.noreply.github.com>
2024-09-03 12:35:52 -0300

    conexao js => html

--- archive.js
+++ archive.js
@@ -1,5 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca. forEach(button =>{
-    button.add
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
 })
